f246b36d3632add67b62a03f4d278247cd8f8667
ShotaroTerato <syoutarou41211@gmail.com>
2015-06-14 15:50:51 +0900

    -

--- src/laser_to_pointcloud/src/laser_to_pointcloud.cpp
+++ src/laser_to_pointcloud/src/laser_to_pointcloud.cpp
@@ -11,40 +11,35 @@
 #include <tf/transform_broadcaster.h>
 #include <tf/transform_listener.h>
 #include <geometry_msgs/Pose.h>
-#include <vector>
-
 
 laser_geometry::LaserProjection projector_;
-tf::TransformListener listener_;
+//tf::TransformListener listener_;
 ros::Publisher pub;
 
 void scanCallback (const sensor_msgs::LaserScan::ConstPtr& scan_in)
 {
-    if(!listener_.waitForTransform(
+ /*   if(!listener_.waitForTransform(
                 scan_in->header.frame_id,
                 "/base_link",
                 scan_in->header.stamp + ros::Duration().fromSec(scan_in->ranges.size()*scan_in->time_increment),
                 ros::Duration(1.0))){
         return;
-    }
+    }*/
 
     sensor_msgs::PointCloud cloud;
-    projector_.transformLaserScanToPointCloud("/base_link",*scan_in,cloud,listener_);
+   // projector_.transformLaserScanToPointCloud("/base_link",*scan_in,cloud,listener_);
+    projector_.projectLaser(*scan_in, cloud);
 
     pub.publish (cloud);
-
-    // Do something with cloud.
 }
 
 int main (int argc, char** argv)
 {
     ros::init (argc, argv, "laser_to_pointcloud");
     ros::NodeHandle nh;
-
     ros::Rate loop_rate(40);
 
     ros::Subscriber sub = nh.subscribe ("/scan2", 40, scanCallback);
-
     pub = nh.advertise<sensor_msgs::PointCloud> ("/to_pointcloud", 40, 1);
 
     ros::spin();
