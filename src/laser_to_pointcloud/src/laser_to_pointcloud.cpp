afc291656725b4188a76ab713b90782215ab39e7
ShotaroTerato <syoutarou41211@gmail.com>
2015-06-15 17:19:32 +0900

    -

--- src/laser_to_pointcloud/src/laser_to_pointcloud.cpp
+++ src/laser_to_pointcloud/src/laser_to_pointcloud.cpp
@@ -15,49 +15,57 @@
 
 #include <geometry_msgs/Pose.h>
 
-ros::Publisher pub;
-
-class PoseDrawer
+class My_Filter
 {
     public:
-        PoseDrawer() : tf_(), target_frame_("/velodyne_link")
+        My_Filter();
+        void scanCallback(const sensor_msgs::LaserScan::ConstPtr& scan_in);
+        /*PoseDrawer() : tf_(), target_frame_("/velodyne_link")
     {
         scan_sub_.subscribe(n_, "/scan2", 10);
         tf_filter_ = new tf::MessageFilter<sensor_msgs::LaserScan>(scan_sub_, tf_, target_frame_, 10);
         tf_filter_->registerCallback(boost::bind(&PoseDrawer::scanCallback, this, _1));
     };
-
+*/
     private:
-        message_filters::Subscriber<sensor_msgs::LaserScan> scan_sub_;
-        tf::TransformListener tf_;
-        tf::MessageFilter<sensor_msgs::LaserScan> * tf_filter_;
-        ros::NodeHandle n_;
-        std::string target_frame_;
+       //message_filters::Subscriber<sensor_msgs::LaserScan> scan_sub_;
+        tf::TransformListener tfListener_;
+        //tf::MessageFilter<sensor_msgs::LaserScan> * tf_filter_;
+        ros::NodeHandle node_;
+        //std::string target_frame_;
         laser_geometry::LaserProjection projector_;
-
-        void scanCallback (const sensor_msgs::LaserScan::ConstPtr& scan_in)
-        {
-            if(!tf_.waitForTransform(
-                        scan_in->header.frame_id,
-                        "/base_link",
-                        scan_in->header.stamp + ros::Duration().fromSec(scan_in->ranges.size()*scan_in->time_increment),
-                        ros::Duration(1.0))){
-                return;
-            }
-
-            sensor_msgs::PointCloud cloud;
-            projector_.transformLaserScanToPointCloud("/base_link",*scan_in,cloud,tf_);
-            //projector_.projectLaser(*scan_in, cloud);
-
-            pub.publish (cloud);
-        };
+        ros::Publisher point_cloud_publisher_;
+        ros::Subscriber scan_sub_;
 };
+
+My_Filter::My_Filter(){
+    scan_sub_ = node_.subscribe<sensor_msgs::LaserScan> ("/scan2", 40, &My_Filter::scanCallback, this);
+    point_cloud_publisher_ = node_.advertise<sensor_msgs::PointCloud2> ("/to_cloud", 40, false);
+    tfListener_.setExtrapolationLimit(ros::Duration(0.1));
+}
+
+void My_Filter::scanCallback (const sensor_msgs::LaserScan::ConstPtr& scan_in)
+{
+
+   
+    if(!tfListener_.waitForTransform(
+                scan_in->header.frame_id,
+                "/base_link",
+                scan_in->header.stamp + ros::Duration().fromSec(scan_in->ranges.size()*scan_in->time_increment),
+                ros::Duration(1.0))){
+        return;
+    }
+    sensor_msgs::PointCloud2 cloud;
+    projector_.transformLaserScanToPointCloud("base_link", *scan_in, cloud, tfListener_);
+    point_cloud_publisher_.publish(cloud);
+
+}
+
 int main (int argc, char** argv)
 {
-    ros::init (argc, argv, "laser_to_pointcloud");
-    PoseDrawer pd;
-    ros::NodeHandle n_;
+    ros::init (argc, argv, "my_filter");
+    My_Filter filter;
     //ros::Subscriber sub = nh.subscribe ("/scan2", 40, scanCallback);
-    pub = n_.advertise<sensor_msgs::PointCloud> ("/to_pointcloud", 10, 1);
+    //pub = n_.advertise<sensor_msgs::PointCloud> ("/to_pointcloud", 10, 1);
     ros::spin();
-};
+}
