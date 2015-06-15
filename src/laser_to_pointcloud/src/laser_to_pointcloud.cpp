abc30bb18e3c030c94d089bf803ab948accd7156
ShotaroTerato <syoutarou41211@gmail.com>
2015-06-15 17:45:06 +0900

    -

--- src/laser_to_pointcloud/src/laser_to_pointcloud.cpp
+++ src/laser_to_pointcloud/src/laser_to_pointcloud.cpp
@@ -20,19 +20,10 @@ class My_Filter
     public:
         My_Filter();
         void scanCallback(const sensor_msgs::LaserScan::ConstPtr& scan_in);
-        /*PoseDrawer() : tf_(), target_frame_("/velodyne_link")
-    {
-        scan_sub_.subscribe(n_, "/scan2", 10);
-        tf_filter_ = new tf::MessageFilter<sensor_msgs::LaserScan>(scan_sub_, tf_, target_frame_, 10);
-        tf_filter_->registerCallback(boost::bind(&PoseDrawer::scanCallback, this, _1));
-    };
-*/
+
     private:
-       //message_filters::Subscriber<sensor_msgs::LaserScan> scan_sub_;
         tf::TransformListener tfListener_;
-        //tf::MessageFilter<sensor_msgs::LaserScan> * tf_filter_;
         ros::NodeHandle node_;
-        //std::string target_frame_;
         laser_geometry::LaserProjection projector_;
         ros::Publisher point_cloud_publisher_;
         ros::Subscriber scan_sub_;
@@ -45,12 +36,10 @@ My_Filter::My_Filter(){
 }
 
 void My_Filter::scanCallback (const sensor_msgs::LaserScan::ConstPtr& scan_in)
-{
-
-   
+{  
     if(!tfListener_.waitForTransform(
                 scan_in->header.frame_id,
-                "/base_link",
+                "base_link",
                 scan_in->header.stamp + ros::Duration().fromSec(scan_in->ranges.size()*scan_in->time_increment),
                 ros::Duration(1.0))){
         return;
@@ -65,7 +54,5 @@ int main (int argc, char** argv)
 {
     ros::init (argc, argv, "my_filter");
     My_Filter filter;
-    //ros::Subscriber sub = nh.subscribe ("/scan2", 40, scanCallback);
-    //pub = n_.advertise<sensor_msgs::PointCloud> ("/to_pointcloud", 10, 1);
     ros::spin();
 }
