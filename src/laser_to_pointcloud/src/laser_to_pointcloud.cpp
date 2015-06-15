6b6132ad31c1be84a15f9ac4bebf95d5505c3ded
ShotaroTerato <syoutarou41211@gmail.com>
2015-06-15 14:27:15 +0900

    -

--- src/laser_to_pointcloud/src/laser_to_pointcloud.cpp
+++ src/laser_to_pointcloud/src/laser_to_pointcloud.cpp
@@ -10,37 +10,53 @@
 
 #include <tf/transform_broadcaster.h>
 #include <tf/transform_listener.h>
+#include <tf/message_filter.h>
+#include <message_filters/subscriber.h>
+
 #include <geometry_msgs/Pose.h>
 
-laser_geometry::LaserProjection projector_;
-//tf::TransformListener listener_;
 ros::Publisher pub;
 
-void scanCallback (const sensor_msgs::LaserScan::ConstPtr& scan_in)
+class PoseDrawer
 {
- /*   if(!listener_.waitForTransform(
-                scan_in->header.frame_id,
-                "/base_link",
-                scan_in->header.stamp + ros::Duration().fromSec(scan_in->ranges.size()*scan_in->time_increment),
-                ros::Duration(1.0))){
-        return;
-    }*/
+    public:
+        PoseDrawer() : tf_(), target_frame_("/scan2")
+    {
+        scan_sub_.subscribe(n_, "/scan2", 10);
+        tf_filter_ = new tf::MessageFilter<sensor_msgs::LaserScan>(scan_sub_, tf_, target_frame_, 10);
+        tf_filter_->registerCallback(boost::bind(&PoseDrawer::scanCallback, this, _1));
+    };
 
-    sensor_msgs::PointCloud cloud;
-   // projector_.transformLaserScanToPointCloud("/base_link",*scan_in,cloud,listener_);
-    projector_.projectLaser(*scan_in, cloud);
+    private:
+        message_filters::Subscriber<sensor_msgs::LaserScan> scan_sub_;
+        tf::TransformListener tf_;
+        tf::MessageFilter<sensor_msgs::LaserScan> * tf_filter_;
+        ros::NodeHandle n_;
+        std::string target_frame_;
+        laser_geometry::LaserProjection projector_;
 
-    pub.publish (cloud);
-}
+        void scanCallback (const sensor_msgs::LaserScan::ConstPtr& scan_in)
+        {
+            if(!tf_.waitForTransform(
+                        scan_in->header.frame_id,
+                        "/base_link",
+                        scan_in->header.stamp + ros::Duration().fromSec(scan_in->ranges.size()*scan_in->time_increment),
+                        ros::Duration(1.0))){
+                return;
+            }
 
+            sensor_msgs::PointCloud cloud;
+            projector_.transformLaserScanToPointCloud("/base_link",*scan_in,cloud,tf_);
+            // projector_.projectLaser(*scan_in, cloud);
+
+            pub.publish (cloud);
+        };
+};
 int main (int argc, char** argv)
 {
     ros::init (argc, argv, "laser_to_pointcloud");
-    ros::NodeHandle nh;
-    ros::Rate loop_rate(40);
-
-    ros::Subscriber sub = nh.subscribe ("/scan2", 40, scanCallback);
-    pub = nh.advertise<sensor_msgs::PointCloud> ("/to_pointcloud", 40, 1);
-
+    PoseDrawer pd;
+    //ros::Subscriber sub = nh.subscribe ("/scan2", 40, scanCallback);
+    //pub = nh.advertise<sensor_msgs::PointCloud> ("/to_pointcloud", 40, 1);
     ros::spin();
-}
+};
